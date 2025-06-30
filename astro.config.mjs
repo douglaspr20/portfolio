a885caab3c2762dcca929b782f0f11dfd2534884
Douglas Pérez <douglas.eduardo2000@gmail.com>
2025-06-30 16:59:05 -0400

    restore pnpm and apply changes in astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,17 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
+import path from "path";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@/": path.resolve(path.dirname(""), "./src"),
+      },
+    },
   },
   integrations: [react()],
   i18n: {
