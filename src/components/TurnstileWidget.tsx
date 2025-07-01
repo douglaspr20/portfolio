ca468693a17467c97950080173d75bd5d9af1712
Douglas Pérez <douglas.eduardo2000@gmail.com>
2025-07-01 16:59:55 -0400

    add contact function, turnstile and nodemailer

--- src/components/TurnstileWidget.tsx
+++ src/components/TurnstileWidget.tsx
@@ -1,19 +1,30 @@
 import { $currentTheme } from "@/store";
-import { Turnstile } from "@marsidev/react-turnstile";
+import { Turnstile, type TurnstileInstance } from "@marsidev/react-turnstile";
 import { useStore } from "@nanostores/react";
-import type { FC } from "react";
+import { useRef, type FC } from "react";
 
 interface Props {
   onSuccess?: (token: string) => void;
 }
 
 const TurnstileWidget: FC<Props> = ({ onSuccess }) => {
+  const ref = useRef<TurnstileInstance | null>(null);
   const currentTheme = useStore($currentTheme);
   return (
     <Turnstile
+      ref={ref}
       siteKey={import.meta.env.PUBLIC_TURNSTILE_SITE_KEY}
-      options={{ theme: currentTheme, size: "flexible" }}
+      options={{
+        theme: currentTheme,
+        size: "flexible",
+        refreshExpired: "manual",
+      }}
       onSuccess={onSuccess}
+      onExpire={() => {
+        if (ref.current) {
+          ref.current.reset();
+        }
+      }}
     />
   );
 };
